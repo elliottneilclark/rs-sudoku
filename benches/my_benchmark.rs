68215ca99fc76b42c95a320f612eb990d6ea18e0
Elliott Clark <elliott.neil.clark@gmail.com>
2019-01-01 21:46:40 -0800

    Add more examples

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,7 +4,9 @@ use rs_sudoku::{parse_sudoku, SolveReport, Solveable};
 
 fn parse_solve(p_str: &str) -> Option<SolveReport> {
     if let Ok(p) = parse_sudoku(p_str) {
-        Some(p.try_solve())
+        let sr = p.try_solve();
+        assert!(sr.is_valid);
+        Some(sr)
     } else {
         None
     }
@@ -13,12 +15,15 @@ fn parse_solve(p_str: &str) -> Option<SolveReport> {
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("easy parse", |b| b.iter(|| parse_solve(ONE_LINE)));
     c.bench_function("multiline parse", |b| b.iter(|| parse_solve(MULTI_LINE)));
-    c.bench_function("multiline hard", |b| {
+    c.bench_function("hard", |b| {
         b.iter(|| {
             parse_solve(
                 "....5..6.9.5..82...4..97.5..64......5.2.8.7.1......63..8.52..1...39..4.2.2..6....",
             )
         })
+    });
+    c.bench_function("parse/solve lots", |b| {
+        b.iter(|| PUZZLES.lines().filter_map(parse_solve).count())
     });
 }
 
